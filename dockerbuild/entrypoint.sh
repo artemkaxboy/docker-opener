58fc0d2d529874112edef1e8df95b6281a91f560
Artem Kolin <artemkaxboy@gmail.com>
2021-07-20 19:50:07 +0700

    Initial commit

---
+++ dockerbuild/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# stop on any error
+set -e
+
+docker ps --filter name=$1
+target_id=$(docker ps --filter name=ctop -ql)
+echo Target container id: $target_id
+
+docker cp /busybox $target_id:/busybox
+
+# runs if any interrupt, terminate, exit signal come. Doesn't work with docker kill
+trap "docker exec -it $target_id /busybin/rm -rf /busybox /busybin" INT TERM EXIT
+
+docker exec -it $target_id /busybox sh -c '
+export PATH="/busybin:$PATH"
+/busybox mkdir /busybin -p
+/busybox --install /busybin
+sh'
