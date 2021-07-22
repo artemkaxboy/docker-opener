9104a6c9751535758916434a124020ed3ad5e419
Artem Kolin <artemkaxboy@gmail.com>
2021-07-22 23:24:13 +0700

    Adds root user to open shell

--- dockerbuild/entrypoint.sh
+++ dockerbuild/entrypoint.sh
@@ -47,9 +47,9 @@ docker ps --filter id="$target_id"
 docker cp /busybox "$target_id":/busybox
 
 # runs if any interrupt, terminate, exit signal come. Doesn't work with docker kill
-trap 'docker exec -it "$target_id" /busybin/rm -rf /busybox /busybin' INT TERM EXIT
+trap 'docker exec --user root -it "$target_id" /busybin/rm -rf /busybox /busybin' INT TERM EXIT
 
-docker exec -it "$target_id" /busybox sh -c '
+docker exec --user root -it "$target_id" /busybox sh -c '
 export PATH="/busybin:$PATH"
 /busybox mkdir /busybin -p
 /busybox --install /busybin
