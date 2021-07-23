8bb540c9682c0bdda68f8612383a9372ad53a536
Artem Kolin <artemkaxboy@gmail.com>
2021-07-23 19:13:03 +0700

    Adds log view command

--- dockerbuild/entrypoint.sh
+++ dockerbuild/entrypoint.sh
@@ -1,56 +1,82 @@
 #!/bin/sh
 
-die () {
+##################### FUNCTIONS
+
+die() {
     echo >&2 "$@"
     exit 1
 }
 
+find_container() {
+    ###### find by exact name
+    target_id=$(docker ps --filter name=^"$1"$ -q)
+    [ -n "$target_id" ] && echo "Found container with name: $1"
+
+    ###### find by name
+    if [ -z "$target_id" ]; then
+        target_id=$(docker ps --no-trunc --format "{{.ID}} {{.Names}}" | grep "\\s.*$1" | head -n1 | awk '{print $1}')
+        [ -n "$target_id" ] && echo "Found container with name containing: $1"
+    fi
+
+    ###### find by image
+    if [ -z "$target_id" ]; then
+        target_id=$(docker ps --no-trunc --format "{{.ID}} {{.Image}}" | grep "\\s.*$1" | head -n1 | awk '{print $1}')
+        [ -n "$target_id" ] && echo "Found container with image containing: $1"
+    fi
+
+    ###### find by id
+    if [ -z "$target_id" ]; then
+        target_id=$(docker ps --no-trunc --format "{{.ID}} {{.ID}}" | grep "\\s.*$1" | head -n1 | awk '{print $1}')
+        [ -n "$target_id" ] && echo "Found container with ID containing: $1"
+    fi
+
+    ###### find by port
+    if [ -z "$target_id" ]; then
+        target_id=$(docker ps --no-trunc --format "{{.ID}} {{.Ports}}" | grep "\\s.*$1" | head -n1 | awk '{print $1}')
+        [ -n "$target_id" ] && echo "Found container with port containing: $1"
+    fi
+
+    if [ -z "$target_id" ]; then
+        die "Container not found: $1"
+    fi
+
+    docker ps --filter id="$target_id"
+}
+
+connect() {
+    [ "$#" -eq 0 ] && die "Target required"
+    [ "$#" -gt 1 ] && die "Only one target expected"
+    find_container "$1"
+    echo Connecting to "$target_id"
+
+    docker cp /busybox "$target_id":/busybox
+
+    # runs if any interrupt, terminate, exit signal come. Doesn't work with docker kill
+    trap 'docker exec --user root -it "$target_id" /busybin/rm -rf /busybox /busybin' INT TERM EXIT
+
+    docker exec --user root -it "$target_id" /busybox sh -c '
+        export PATH="/busybin:$PATH"
+        /busybox mkdir /busybin -p
+        /busybox --install /busybin
+        sh'
+}
+
+fetch_logs() {
+    [ "$#" -eq 0 ] && die "Target required"
+    [ "$#" -gt 1 ] && die "Only one target expected"
+    find_container "$1"
+    echo Fetching logs for "$target_id"
+
+    docker logs -f "$target_id"
+}
+
+##################### START
 # stop on any error
 set -e
 
-[ "$#" -eq 1 ] || die "One argument required"
-
-###### find by exact name
-target_id=$(docker ps --filter name=^"$1"$ -q)
-[ -n "$target_id" ] && echo "Found container with name: $1"
-
-###### find by name
-if [ -z "$target_id" ]; then
-    target_id=$(docker ps --no-trunc --format "{{.ID}} {{.Names}}" | grep "\\s.*$1" | head -n1 | awk '{print $1}')
-    [ -n "$target_id" ] && echo "Found container with name containing: $1"
-fi
-
-###### find by image
-if [ -z "$target_id" ]; then
-    target_id=$(docker ps --no-trunc --format "{{.ID}} {{.Image}}" | grep "\\s.*$1" | head -n1 | awk '{print $1}')
-    [ -n "$target_id" ] && echo "Found container with image containing: $1"
-fi
-
-###### find by id
-if [ -z "$target_id" ]; then
-    target_id=$(docker ps --no-trunc --format "{{.ID}} {{.ID}}" | grep "\\s.*$1" | head -n1 | awk '{print $1}')
-    [ -n "$target_id" ] && echo "Found container with ID containing: $1"
-fi
-
-###### find by port
-if [ -z "$target_id" ]; then
-    target_id=$(docker ps --no-trunc --format "{{.ID}} {{.Ports}}" | grep "\\s.*$1" | head -n1 | awk '{print $1}')
-    [ -n "$target_id" ] && echo "Found container with port containing: $1"
-fi
-
-if [ -z "$target_id" ]; then
-    die "Container not found: $1"
-fi
-
-docker ps --filter id="$target_id"
-
-docker cp /busybox "$target_id":/busybox
-
-# runs if any interrupt, terminate, exit signal come. Doesn't work with docker kill
-trap 'docker exec --user root -it "$target_id" /busybin/rm -rf /busybox /busybin' INT TERM EXIT
-
-docker exec --user root -it "$target_id" /busybox sh -c '
-export PATH="/busybin:$PATH"
-/busybox mkdir /busybin -p
-/busybox --install /busybin
-sh'
+case $1 in
+    logs)
+        shift
+        fetch_logs "$@" ;;
+    *) connect "$@" ;;
+esac
