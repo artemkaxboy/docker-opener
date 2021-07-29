1bcdedb93ad1ecfbc625d6aff5279e15aaec22a2
Artem Kolin <artemkaxboy@gmail.com>
2021-07-29 19:30:01 +0700

    Migrates to python

---
+++ dockerbuild/python/commands/compose.py
@@ -0,0 +1,66 @@
+from tools import system_tools
+from tools.docker_tools import docker_ps, get_compose_name, make_fake_compose, fake_compose_path
+
+compose_project = "docker-compose --project-name %s --file "
+compose_logs_command = compose_project + fake_compose_path + " logs %s"
+compose_kill_command = compose_project + fake_compose_path + " kill %s"
+compose_down_command = compose_project + fake_compose_path + " down %s"
+compose_start_command = compose_project + fake_compose_path + " start %s"
+compose_stop_command = compose_project + fake_compose_path + " stop %s"
+compose_top_command = compose_project + fake_compose_path + " top %s"
+
+
+def prepare_command(args, command, search_all=False):
+    if len(args) == 0:
+        system_tools.die("Target required")
+
+    target = args[0]
+
+    target_name = get_compose_name(target, search_all)
+    docker_ps(compose_name=target_name, search_all=search_all)
+
+    make_fake_compose(target_name, search_all=search_all)
+    arg_string = ' '.join(args[1:])
+    system_tools.prepare_command(command % (target_name, arg_string))
+
+
+def logs(args):
+    if len(args) == 0:
+        system_tools.die("Compose logs target required")
+
+    prepare_command(args, compose_logs_command)
+
+
+def kill(args):
+    if len(args) == 0:
+        system_tools.die("Compose kill target required")
+
+    prepare_command(args, compose_kill_command)
+
+
+def down(args):
+    if len(args) == 0:
+        system_tools.die("Compose down target required")
+
+    prepare_command(args, compose_down_command)
+
+
+def start(args):
+    if len(args) == 0:
+        system_tools.die("Compose start target required")
+
+    prepare_command(args, compose_start_command, search_all=True)
+
+
+def stop(args):
+    if len(args) == 0:
+        system_tools.die("Compose stop target required")
+
+    prepare_command(args, compose_stop_command)
+
+
+def top(args):
+    if len(args) == 0:
+        system_tools.die("Compose top target required")
+
+    prepare_command(args, compose_top_command)
---
+++ dockerbuild/python/tools/system_tools.py
@@ -0,0 +1,44 @@
+import subprocess
+import sys
+
+
+def die(*args):
+    print('\n'.join(args))
+    sys.exit(1)
+
+
+def os_run(*command, stdout=True):
+    """
+    Run given command.
+    :param command: command to run
+    :param stdout: prints output to stdout if true, otherwise - returns output
+    :return: output or None
+    """
+    to_run = command[0]
+
+    test = subprocess.Popen(to_run, stdout=subprocess.PIPE)
+    output = test.communicate()[0].decode("utf-8")
+
+    if stdout:
+        print("%s" % output)
+    else:
+        return output
+
+
+def divide_target_and_options(args):
+    if len(args) == 0:
+        raise ValueError("Logs target required!")
+    elif len(args) == 1:
+        if args[0][0] == '-':
+            raise ValueError("Logs target required!")
+        return args[0], []
+    else:
+        if args[0][0] == '-':
+            return args[-1], args[0:-1]
+        return args[0], args[1:]
+
+
+def prepare_command(command):
+    file_name = "command.sh"
+    with open(file_name, "w") as file:
+        file.write(command)
