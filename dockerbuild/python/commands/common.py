51d7c2680b771be5d480775faa3f9baa23cf46ca
Artem Kolin <artemkaxboy@gmail.com>
2021-08-03 00:26:22 +0700

    Adds `version` command

---
+++ dockerbuild/python/commands/common.py
@@ -0,0 +1,64 @@
+import os
+import socket
+
+from docker import client
+from docker.errors import NotFound, APIError
+from docker.models.images import Image
+
+help_msg = """
+Usage: COMMAND [OPTIONS]
+
+Commands:
+  --                 Attach terminal to running container
+  cd, compose-down   Stop and remove compose resources
+  cl, compose-logs   View output from compose containers
+  ck, compose-kill   Kill compose containers
+      compose-start  Start compose services
+      compose-stop   Stop compose services
+  ct, compose-top    Display the running compose processes
+  h,  help           Show help message
+  l,  logs           Fetch container logs
+  u,  update         Update (pull) current image
+
+To get more help with opener, check out our docs at https://github.com/artemkaxboy/docker-opener
+"""
+
+
+# noinspection PyShadowingBuiltins
+def help():
+    """
+    Prints help message.
+    :return: None
+    """
+    print(help_msg)
+
+
+def update():
+    """
+    Updates current image tags.
+    :return: None
+    :raises: ValueError - cannot find current container image name
+    """
+    hostname = socket.gethostname()
+    docker = client.from_env()
+    try:
+        current_image: Image = docker.containers.get(hostname).image
+    except NotFound:
+        raise ValueError("Current container image not found")
+
+    for tag in current_image.tags:
+        try:
+            print("\nPulling %s..." % tag)
+            docker.images.pull(tag)
+            print("OK")
+        except APIError as e:
+            print("error: %s" % e)
+
+
+def version():
+    """
+    Prints current image version.
+    :return: None
+    """
+    print("Version: %s" % os.environ.get("VERSION"))
+    print("Revision: %s" % os.environ.get("REVISION"))
--- dockerbuild/python/make_command.py
+++ dockerbuild/python/make_command.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 import sys
 
-from commands import attach, compose, help, logs, update
+from commands import attach, compose, logs, common
 from tools import system_tools, docker_tools
 
-update_commands = ["u", "update"]
+# ------------------------------------ common commands
 help_commands = ["h", "-h", "help", "--help"]
-logs_commands = ["l", "logs"]
+update_commands = ["u", "update"]
+version_commands = ["v", "-v", "version", "--version"]
+
+# ------------------------------------ compose commands
 compose_down_commands = ["cd", "cdown", "compose-down"]
-compose_logs_commands = ["cl", "clogs", "compose-logs"]
 compose_kill_commands = ["ck", "ckill", "compose-kill"]
+compose_logs_commands = ["cl", "clogs", "compose-logs"]
 compose_start_commands = ["cstart", "compose-start"]
 compose_stop_commands = ["cstop", "compose-stop"]
 compose_top_commands = ["ct", "ctop", "compose-top"]
+
+# ------------------------------------ container commands
 attach_commands = ["--"]
+logs_commands = ["l", "logs"]
 
 try:
     if not docker_tools.is_docker_available():
@@ -21,14 +27,16 @@ try:
 
     args = sys.argv[1:]
     if len(args) == 0:
-        system_tools.die("Target required")
+        raise ValueError("Target required")
 
     command = args[0]
 
     if command in help_commands:
-        help.run()
+        common.help()
     elif command in update_commands:
-        update.run()
+        common.update()
+    elif command in version_commands:
+        common.version()
     elif command in logs_commands:
         logs.run(args[1:])
     elif command in compose_down_commands:
@@ -43,6 +51,7 @@ try:
         compose.stop(args[1:])
     elif command in compose_top_commands:
         compose.top(args[1:])
+
     elif command in attach_commands:
         attach.run(args[1:])
     else:
