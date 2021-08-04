6c7baa49014a0b83826eb961df557940b231f2b3
Artem Kolin <artemkaxboy@gmail.com>
2021-08-05 00:07:03 +0700

    Adds `recreate`, `upgrade` commands

--- dockerbuild/python/commands/container.py
+++ dockerbuild/python/commands/container.py
@@ -1,5 +1,4 @@
-from tools import docker_tools
-from tools import system_tools
+from tools import docker_container_tools, docker_common_tools, docker_tools, docker_image_tools, system_tools
 
 connect_command = "docker exec --user 0 -it %s %s"
 
@@ -14,8 +13,8 @@ def attach(args):
     if len(args) != 1:
         raise ValueError("Exactly 1 target required")
 
-    target_id = docker_tools.get_container_id(args[0])
-    docker_tools.docker_ps(target_id)
+    target_id = docker_container_tools.find_container_id(args[0])
+    docker_common_tools.docker_ps(container_ids=[target_id])
 
     interpreter = docker_tools.get_interpreter(target_id)
     print("Connecting...")
@@ -33,26 +32,75 @@ def logs(args):
         raise ValueError("Logs target required")
 
     target, options = system_tools.divide_target_and_options(args)
-    target_id = docker_tools.get_container_id(target)
-    docker_tools.docker_ps(container_id=target_id)
+    target_id = docker_container_tools.find_container_id(target)
+    docker_common_tools.docker_ps(container_ids=[target_id])
 
     system_tools.prepare_command("docker logs %s %s" % (target_id, options))
 
 
 def kill(args):
     """
-    Prepares command to kill container.
+    Prepares command to kill containers.
     :param args: array of command args, must have attribute to find container at first or last place
     :return: None
     :raises ValueError if no target in args
     :raises OSError if non interactive mode
     """
     if len(args) == 0:
-        raise ValueError("Logs target required")
+        raise ValueError("Kill target required")
 
     target, options = system_tools.divide_target_and_options(args)
-    target_id = docker_tools.get_container_id(target)
-    docker_tools.docker_ps(container_id=target_id)
+    target_container_ids = docker_container_tools.find_container_ids(target, raise_if_not_found=True)
 
-    if system_tools.get_consent("You are killing container `%s`" % docker_tools.get_container_name(target_id)):
-        system_tools.prepare_command("docker kill %s %s" % (target_id, options))
+    docker_common_tools.docker_ps(container_ids=target_container_ids)
+
+    if system_tools.get_consent("You are killing %d container(s)" % len(target_container_ids)):
+        commands = '\n'.join(map(lambda target_id: "docker kill %s %s" %
+                                                   (target_id, options), target_container_ids))
+        system_tools.prepare_command(commands)
+
+
+def upgrade(args):
+    """
+    Recreates container with existing parameters and prepares command to start the new one.
+    Pulls image before recreating.
+    :param args: array of command args, must have attribute to find container at first or last place
+    :return: None
+    :raises ValueError if no target in args
+    :raises OSError if non interactive mode
+    """
+    recreate(args, need_upgrade=True)
+
+
+def recreate(args, need_upgrade: bool = False):
+    """
+    Recreates container with existing parameters and prepares command to start the new one.
+    :param need_upgrade: pull image before recreating
+    :param args: array of command args, must have attribute to find container at first or last place
+    :return: None
+    :raises ValueError if no target in args, if no target or more than one target found
+    :raises OSError if non interactive mode
+    """
+    if len(args) == 0:
+        raise ValueError("Recreate target required")
+
+    if len(args) != 1:
+        raise ValueError("Command has no options")
+
+    target_container = docker_container_tools.find_container(args[0])
+    target_container_id = target_container.id
+    target_container_name = target_container.name
+    target_container_image = docker_image_tools.get_image_name(target_container)
+
+    docker_common_tools.docker_ps(container_ids=[target_container_id])
+
+    if need_upgrade:
+        docker_image_tools.pull_image(target_container_image)
+
+    print("Recreating `%s` ..." % target_container_name)
+
+    new_container_id = docker_container_tools.copy_container(target_container_id)
+    docker_container_tools.stop_container(target_container_id)
+    docker_container_tools.remove_container(target_container_id)
+    docker_container_tools.rename_container(new_container_id, target_container_name)
+    docker_container_tools.start_container(new_container_id)
---
+++ dockerbuild/python/tools/docker_common_tools.py
@@ -0,0 +1,47 @@
+from typing import List
+
+import docker
+from docker import DockerClient
+from docker.errors import DockerException
+
+from tools.system_tools import os_run
+
+compose_project_label = "com.docker.compose.project"
+
+
+def get_docker() -> DockerClient:
+    """
+    Returns docker client.
+    :return: DockerClient from environment
+    """
+    return docker.client.from_env()
+
+
+def docker_ps(container_ids: List[str] = None, compose_name: str = None, search_all=False):
+    """
+    Prints docker ps command output.
+    :param container_ids: prints only given containers if any
+    :param compose_name: prints only given compose containers if given
+    :param search_all: include stopped containers
+    :return: None
+    """
+    all_command = ["--all"] if search_all else []
+    if container_ids is None and compose_name is None:
+        os_run(["docker", "ps"] + all_command)
+    elif container_ids is not None:
+        filters = []
+        for container_id in container_ids:
+            filters.append("--filter")
+            filters.append("id=%s" % container_id)
+        os_run(["docker", "ps"] + filters + all_command)
+    else:
+        os_run(
+            ["docker", "ps", "--filter", "label=" + compose_project_label + "=" + compose_name] + all_command)
+
+
+def is_docker_available():
+    try:
+        docker.client.from_env()
+        return True
+    except DockerException:
+        return False
